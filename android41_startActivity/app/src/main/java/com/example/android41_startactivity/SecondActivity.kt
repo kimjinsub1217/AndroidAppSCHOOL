a7f26ca0a2b7a61f2ad3de75e94c4cfac89dba51
kimjinsub1217 <hikm9985@naver.com>
2023-06-16 17:47:14 +0900

    230616 수업내용

---
+++ android41_startActivity/app/src/main/java/com/example/android41_startactivity/SecondActivity.kt
@@ -0,0 +1,16 @@
+
+
+
+
+
+package com.example.android41_startactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
