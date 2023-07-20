af27f984e26b72652ba0ef0c61393f156027a8f4
kimjinsub1217 <hikm9985@naver.com>
2023-07-20 22:42:12 +0900

    미니프로젝트01 - 2

--- mini01_LBS01/app/src/main/java/com/example/mini01_lbs01/MapActivity.kt
+++ mini01_LBS01/app/src/main/java/com/example/mini01_lbs01/MapActivity.kt
@@ -2,6 +2,7 @@ package com.example.mini01_lbs01
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.content.pm.PackageManager.PERMISSION_GRANTED
 import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
@@ -12,6 +13,7 @@ import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.mini01_lbs01.BuildConfig.MAPS_API_KEY2
 import com.example.mini01_lbs01.databinding.ActivityMapBinding
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
@@ -25,11 +27,45 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.model.RectangularBounds
+import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest
+import com.google.android.libraries.places.api.net.PlacesClient
+import com.google.android.gms.maps.model.LatLngBounds
 
 class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+    private val LOCATION_PERMISSION_REQUEST_CODE = 1
 
     lateinit var activityMainBinding: ActivityMapBinding
     lateinit var googleMap: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var placesClient: PlacesClient
+
+    val dialogData = arrayOf(
+        "accounting", "airport", "amusement_park",
+        "aquarium", "art_gallery", "atm", "bakery",
+        "bank", "bar", "beauty_salon", "bicycle_store",
+        "book_store", "bowling_alley", "bus_station",
+        "cafe", "campground", "car_dealer", "car_rental",
+        "car_repair", "car_wash", "casino", "cemetery",
+        "church", "city_hall", "clothing_store", "convenience_store",
+        "courthouse", "dentist", "department_store", "doctor",
+        "drugstore", "electrician", "electronics_store", "embassy",
+        "fire_station", "florist", "funeral_home", "furniture_store",
+        "gas_station", "gym", "hair_care", "hardware_store", "hindu_temple",
+        "home_goods_store", "hospital", "insurance_agency",
+        "jewelry_store", "laundry", "lawyer", "library", "light_rail_station",
+        "liquor_store", "local_government_office", "locksmith", "lodging",
+        "meal_delivery", "meal_takeaway", "mosque", "movie_rental", "movie_theater",
+        "moving_company", "museum", "night_club", "painter", "park", "parking",
+        "pet_store", "pharmacy", "physiotherapist", "plumber", "police", "post_office",
+        "primary_school", "real_estate_agency", "restaurant", "roofing_contractor",
+        "rv_park", "school", "secondary_school", "shoe_store", "shopping_mall",
+        "spa", "stadium", "storage", "store", "subway_station", "supermarket",
+        "synagogue", "taxi_stand", "tourist_attraction", "train_station",
+        "transit_station", "travel_agency", "university", "eterinary_care","zoo"
+    )
 
     val permissions = arrayOf(
         Manifest.permission.ACCESS_FINE_LOCATION,
@@ -47,6 +83,34 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
         activityMainBinding = ActivityMapBinding.inflate(layoutInflater)
         setContentView(activityMainBinding.root)
+
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        Places.initialize(applicationContext, MAPS_API_KEY2)
+        placesClient = Places.createClient(this)
+
+        // 위치 권한 확인
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            // 사용자 위치 가져오기
+            fetchLastLocation()
+        } else {
+            // 위치 권한 요청
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_PERMISSION_REQUEST_CODE
+            )
+        }
+
+
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
 
         if (isPermitted()) {
             startProcess()
@@ -71,6 +135,7 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
             }
         }
     }
+
 
     fun isPermitted(): Boolean {
         for (perm in permissions) {
@@ -98,7 +163,6 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     // 내 위치를 가져오기
-    lateinit var fusedLocationClient: FusedLocationProviderClient
     lateinit var locationCallback: LocationCallback
 
     @SuppressLint("MissingPermission")
@@ -169,4 +233,52 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
             }
         }
     }
+
+    private fun fetchLastLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener(this) { location ->
+                if (location != null) {
+                    // 현재 위치 좌표
+                    val currentLatLng = LatLng(location.latitude, location.longitude)
+
+                    // Place API를 사용하여 주변 ATM 검색
+                    findNearbyBookStores(currentLatLng)
+                }
+            }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun findNearbyBookStores(location: LatLng) {
+
+        val placeFields: List<Place.Field> = listOf(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG, Place.Field.TYPES)
+        val request = FindCurrentPlaceRequest.builder(placeFields).build()
+        val placeResponse = placesClient.findCurrentPlace(request)
+
+        placeResponse.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val response = task.result
+                if (response != null) {
+                    for (placeLikelihood in response.placeLikelihoods) {
+                        val place = placeLikelihood.place
+                        if (place.types != null && place.types!!.contains(Place.Type.HOME_GOODS_STORE)) {
+                            val placeLatLng = place.latLng
+                            val placeName = place.name
+                            googleMap.addMarker(MarkerOptions().position(placeLatLng!!).title(placeName))
+                        }
+                    }
+                }
+            } else {
+                // API 호출에 실패했을 경우 처리할 내용
+            }
+        }
+    }
+
 }
